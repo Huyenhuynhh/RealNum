0f9b05f21fa664a8f01e403457401cd4c8fa5217
Huyenhuynhh <huynhh4@vcu.edu>
2021-11-22 07:58:24 -0800

    complete the signup and login screen design

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+  EditText email,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        password=findViewById(R.id.password);
+        email=findViewById(R.id.email);
+    }
+
+    public void createAccount(View view) {
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+    }
+
+    public void loginAccount(View view) {
+        if(!(email.getText().toString().equals("")&&password.getText().toString().equals(""))){
+            startActivity(new Intent(MainActivity.this,WelcomeActivity.class));
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/login/SignUpActivity.java
@@ -0,0 +1,29 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText email,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        password=findViewById(R.id.tpassword);
+        email=findViewById(R.id.temail);
+    }
+
+    public void createAccount(View view) {
+        if(!(email.getText().toString().equals("")&&password.getText().toString().equals(""))){
+            startActivity(new Intent(SignUpActivity.this,WelcomeActivity.class));
+        }
+    }
+
+    public void moveToLoginScreen(View view) {
+        startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+    }
+}
