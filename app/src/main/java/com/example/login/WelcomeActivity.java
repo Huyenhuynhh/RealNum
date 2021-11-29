e177a421205d7e37ac7fcf727bc9e22cc76824a7
Snowman4LFE <snowman4lfe@gmail.com>
2021-11-28 22:50:54 -0500

    Adding onClick Function Need to change welcome activity to fragment

--- app/src/main/java/com/example/login/WelcomeActivity.java
+++ app/src/main/java/com/example/login/WelcomeActivity.java
@@ -1,8 +1,15 @@
 package com.example.login;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +17,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        final Button trackingButton = (Button) findViewById(R.id.button_tracking);
+
+        trackingButton.setOnClickListener(v -> Navigation.findNavController(WelcomeActivity.this,R.id.nav_controller_view_tag)
+                .navigate(R.id.action_welcomeActivity_to_trackingActivity));
+
     }
 }
