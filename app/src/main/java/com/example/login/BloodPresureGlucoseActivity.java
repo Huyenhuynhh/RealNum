fe054c1614216be6b605526300812e608b07f874
Huyenhuynhh <huynhh4@vcu.edu>
2021-11-28 21:42:23 -0800

    design the screen when user select week or month then display the list accordingly

--- app/src/main/java/com/example/login/BloodPresureGlucoseActivity.java
+++ app/src/main/java/com/example/login/BloodPresureGlucoseActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class BloodPresureGlucoseActivity extends AppCompatActivity {
       TextView title,daytext,mothtext,weektext,datatext;
+      Button addbtn;
+      ListView listview;
+    String[] weekArray = {"Monday","Tuesday","Wednesday","Thursday",
+            "Friday","Saturday","Sunday"};
+    String[] monthArray = {"January","February","March","April",
+            "May","June","July","August","September","October","November","December"};
+    ArrayAdapter weekAdapter,mothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +28,17 @@ public class BloodPresureGlucoseActivity extends AppCompatActivity {
         weektext=findViewById(R.id.weektext);
         daytext.setBackgroundResource(R.drawable.text_bg);
         datatext=findViewById(R.id.datatext);
+        addbtn=findViewById(R.id.addbtn);
+        listview=findViewById(R.id.listview);
+        weekAdapter = new ArrayAdapter<String>(this,
+                R.layout.item_list, weekArray);
+        mothAdapter = new ArrayAdapter<String>(this,
+                R.layout.item_list, monthArray);
+        weektext.setBackgroundResource(0);
+        mothtext.setBackgroundResource(0);
+        addbtn.setVisibility(View.VISIBLE);
+        datatext.setVisibility(View.VISIBLE);
+        listview.setVisibility(View.GONE);
 
     }
 
@@ -35,17 +56,28 @@ public class BloodPresureGlucoseActivity extends AppCompatActivity {
         daytext.setBackgroundResource(R.drawable.text_bg);
         weektext.setBackgroundResource(0);
         mothtext.setBackgroundResource(0);
+        addbtn.setVisibility(View.VISIBLE);
+        datatext.setVisibility(View.VISIBLE);
+        listview.setVisibility(View.GONE);
     }
 
     public void weekTextClicked(View view) {
         weektext.setBackgroundResource(R.drawable.text_bg);
         daytext.setBackgroundResource(0);
         mothtext.setBackgroundResource(0);
+        addbtn.setVisibility(View.GONE);
+        datatext.setVisibility(View.GONE);
+        listview.setAdapter(weekAdapter);
+        listview.setVisibility(View.VISIBLE);
     }
 
     public void mothTextClicked(View view) {
         mothtext.setBackgroundResource(R.drawable.text_bg);
         weektext.setBackgroundResource(0);
         daytext.setBackgroundResource(0);
+        addbtn.setVisibility(View.GONE);
+        datatext.setVisibility(View.GONE);
+        listview.setAdapter(mothAdapter);
+        listview.setVisibility(View.VISIBLE);
     }
 }
